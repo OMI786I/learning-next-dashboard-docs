a56ab5ed5c5526e910e5f85b5c14b2c3a41df508
OMI786I <sohrawardy1998@gmail.com>
2024-10-28 02:41:04 +0600

    made the landing pge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,40 @@
 import Image from "next/image";
 import styles from "../../src/app/ui/home.module.css";
 import { lusitana } from "./ui/font";
+import { BsGlobe } from "react-icons/bs";
+import { BiArrowFromRight } from "react-icons/bi";
 export default function Home() {
   console.log(lusitana);
   return (
     <>
-      <div className={styles.shape}></div>
-      <p>Checking fonts(This is inter font which is primary font)</p>
-      <p className={`${lusitana.className}`}>This is lusitana</p>
+      <div className="bg-blue-500 text-white h-48 rounded-lg flex items-end">
+        <span className="flex items-center gap-1">
+          <BsGlobe className="text-xl" /> <p className="text-2xl">Acme</p>
+        </span>
+      </div>
+      <div className="grid grid-cols-2 items-center">
+        <div className="bg-blue-100 h-full p-24 flex flex-col justify-center items-start gap-2">
+          <div className="text-lg">
+            <span className="font-bold">Welcome to Acme.</span>This is the
+            example for the{" "}
+            <span className="text-blue-500">Next.js Learn Course,</span>brought
+            to you by Vercel
+          </div>
 
-      <Image
-        src={"/hero-desktop.png"}
-        width={1000}
-        height={600}
-        alt="A screen shot"
-      />
+          <button className="btn btn-info">
+            Login in <BiArrowFromRight />
+          </button>
+        </div>
+        <div>
+          {" "}
+          <Image
+            src={"/hero-desktop.png"}
+            width={700}
+            height={400}
+            alt="A screen shot"
+          />
+        </div>
+      </div>
     </>
   );
 }
